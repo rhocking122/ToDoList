b13febf0fac191dd62aa6c7ebc9a143975575590
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-08-28 12:30:53 -0500

    created items section

--- todoList/App.js
+++ todoList/App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './Components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+ 
+{/* Today's Tasks */}
+    <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+    <View style={styles.items}>
+      {/* This is where the tasks go */}
+      <Task />
+      <Task />
+      <Task />
+    </View>
+
+    </View>
+
     </View>
   );
 }
@@ -13,8 +26,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#EBEAED',
   },
+tasksWrapper: {
+  paddingTop: 80,
+  paddingHorizontal: 20,
+},
+sectionTitle: {
+  fontSize: 24,
+  fontWeight: 'bold',
+},
+items: {},
 });
